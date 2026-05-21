822f4172f2a43222bd908e075c56799a213c2c5a
radikawidip <radikawidip@gmail.com>
2026-05-21 20:09:30 +0700

    Update firebase-config.js

--- components/firebase-config.js
+++ components/firebase-config.js
@@ -1,11 +1,14 @@
 // TODO: Ganti placeholder di bawah ini dengan Firebase Config dari Firebase Console Anda
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBSN2t7aUBhIGnKFeon4Q4YFcKp-_iCmYc",
+  authDomain: "task-flow-e7854.firebaseapp.com",
+  databaseURL: "https://task-flow-e7854-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "task-flow-e7854",
+  storageBucket: "task-flow-e7854.firebasestorage.app",
+  messagingSenderId: "470421738597",
+  appId: "1:470421738597:web:b4025d5119e34150e5628c",
+  measurementId: "G-RMHDW3CKHJ"
 };
 
 // Inisialisasi Firebase
